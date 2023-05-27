4ad89adcc357ab8db77c0fcbe068c4abbe2642a5
13awcs <bawcs0206@gmail.com>
2023-05-27 12:36:49 +0700

    convert mono to micro

--- recruit-service/src/main/java/com/example/recruitservice/controller/RecruiterController.java
+++ recruit-service/src/main/java/com/example/recruitservice/controller/RecruiterController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("recruit/recruiter")
+@RequestMapping("/recruiter")
 @RequiredArgsConstructor
 public class RecruiterController {
     private final RecruiterService recruiterService;
